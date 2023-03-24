f1bdb43485dcfac4bdf4e78e84945e4fe11bf7e8
Denys Ovsiienko <dovsiien@gmail.com>
2023-03-23 18:56:50 -0700

    Fixed image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, description, image }) => {
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg bg-white">
-      {image && <Image className="w-full" src={image} alt={title} />}
+      {image && <img className="w-full" src={image} alt={title} />}
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-gray-700 text-base">{description}</p>
